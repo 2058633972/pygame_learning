a3a29437671525ad77f79871365cd0663bd44b45
qinhaicheng <helloqinhaicheng.com>
2020-08-09 16:52:53 +0800

    展示型

--- BallGame/BallGame.py
+++ BallGame/BallGame.py
@@ -1,9 +1,22 @@
 #Author:Haicheng Qin
 import pygame,sys
-screen=pygame.display.set_mode((600,400))
-pygame.display.set_caption("pygame游戏之旅")
+pygame.init()
+size = width, height = 600, 400
+speed = [1, 1]
+black = 0, 0, 0
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("壁球小游戏展示型")
+ball = pygame.image.load("BallGame/ball.gif")
+ballrect = ball.get_rect()
 while True:
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
+        if event.type == pygame.QUIT:
             sys.exit()
-        pygame.display.update()
+    ballrect = ballrect.move(speed[0],speed[1])
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.display.update()
