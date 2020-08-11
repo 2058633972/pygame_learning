692a0042b1c5aa91ae3c5600f1db1f3521aa127f
qinhaicheng <helloqinhaicheng.com>
2020-08-11 09:06:41 +0800

    操控型

--- BallGame/BallGame.py
+++ BallGame/BallGame.py
@@ -5,16 +5,25 @@ size = width, height = 600, 400
 speed = [1, 1]
 black = 0, 0, 0
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("壁球小游戏节奏型")
+pygame.display.set_caption("操控型")
 ball = pygame.image.load("ball.gif")
 ballrect = ball.get_rect()
-fps=300
-fclock=pygame.time.Clock()
+fps = 300
+fclock = pygame.time.Clock()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    ballrect = ballrect.move(speed[0],speed[1])
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                speed[0]=speed[0] if speed[0] == 0 else (abs(speed[0])-1)*int(speed[0]/abs(speed[0]))
+            elif event.key == pygame.K_RIGHT:
+                speed[0] = speed[0]+1 if speed[0] > 0 else speed[0]-1
+            elif event.key == pygame.K_DOWN:
+                speed[1] = speed[1] if speed[1] == 0 else (abs(speed[1])-1)*int(speed[1]/abs(speed[1]))
+            elif event.key == pygame.K_UP:
+                speed[1] = speed[1]+1 if speed[1] > 0 else speed[1]-1
+    ballrect = ballrect.move(speed)
     if ballrect.left < 0 or ballrect.right > width:
         speed[0] = -speed[0]
     if ballrect.top < 0 or ballrect.bottom > height:
